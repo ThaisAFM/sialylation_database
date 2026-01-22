8022cf814938c058acc1e7f0b147cd2d08081557
ThaisAFM <thaisalanaf@usp.br>
2026-01-22 10:53:51 -0300

    Create interproscan.sh

---
+++ interproscan.sh
@@ -0,0 +1,5 @@
+# InterProScan v5.76 was installed according to the official documentation,
+# available at: https://interproscan-docs.readthedocs.io/en/v5/HowToDownload.html,
+# and was executed locally
+
+interproscan.sh -i sialylation_database_nr.fasta -o sialylation_interpro -f tsv -dp
