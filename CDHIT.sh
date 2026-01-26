6547af686069e6fb33456709f3198fda0c89c577
ThaisAFM <thaisalanaf@usp.br>
2026-01-26 15:40:10 -0300

    Update CDHIT.sh

--- CDHIT.sh
+++ CDHIT.sh
@@ -2,4 +2,8 @@
 # A `.clstr` file containing the clusters of redundant sequences at **100% identity**.  
 # A FASTA file containing only the representative sequence for each cluster.
 
-cd-hit -i sialylation_database.fasta -o sialylation_database_nr.fasta -c 1.00 -n 5 -M 2000
+for arquivo in *.fasta; do
+    saida="${arquivo%.fasta}_nr"
+    cd-hit -i "$arquivo" -o "$saida" -c 1.00 -n 5 -M 2000
+    echo "Processado: $arquivo -> ${saida}"
+  done
